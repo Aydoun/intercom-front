a647879ddd671a1bbaf1d7c812f895b15417e2d0
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-08-05 21:59:49 +0200

    feat(Header): Redesign app's Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Link } from 'react-router-dom';
+import { Icon, Input } from 'antd';
+import logo from '../../logo.svg';
 
-const { Header } = Layout;
+const { Search } = Input;
+
 
 export default () => (
-    <Header className="app__header">
-        <div className="app__header-text">
-            Brand
-        </div> 
-    </Header>
+    <div className="header">
+        <div className="app__logo">
+            <img src={logo} alt="app-logo"/>
+            <Search
+                placeholder="input search text"
+                onSearch={value => console.log(value)}
+                style={{ width: 200, marginLeft: 8 }}
+            />
+        </div>
+        <div className="header__menu">
+        
+            <Link to="/messages"><Icon type="message" className="header__menu-icon" /></Link>
+            <Link to="/messages"><Icon type="notification" className="header__menu-icon" /></Link>
+            
+        </div>
+        
+    </div>
 );
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
 import Header from '../Header';
-import { Layout, Menu, Icon } from 'antd';
-
-const { Sider, Content } = Layout;
 
 export default ({ children }) => (
-    <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsed theme="light" reverseArrow >
-            <div className="app__logo" />
-            <Menu defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1">
-                    <Icon type="pie-chart" />
-                    <span>Option 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                    <Icon type="desktop" />
-                    <span>Option 2</span>
-                </Menu.Item>
-                <Menu.Item key="9">
-                    <Icon type="file" />
-                    <span>File</span>
-                </Menu.Item>
-            </Menu>
-        </Sider>
-        <Layout>
-            <Content >
-                <Header />
-                <div className="app__wrapper">
-                    { children }
-                </div>
-            </Content>
-        </Layout>
-    </Layout>
+    <div>
+        <Header />
+        <div className="app__wrapper">
+            { children }
+        </div>
+    </div>
 );
