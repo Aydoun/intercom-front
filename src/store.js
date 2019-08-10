a64a371783a5b8aa1bd3d5b89e7cd6ec854816b8
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-08-10 13:33:55 +0200

    chore: Add redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reduxLogger from 'redux-logger';
 import mySaga from './sagas';
@@ -6,7 +6,10 @@ import rootReducer from './reducers';
 
 const defaultState = {};
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, defaultState, applyMiddleware(sagaMiddleware, reduxLogger));
+const widthDevTools = compose(applyMiddleware(sagaMiddleware, reduxLogger), 
+                      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(rootReducer, defaultState, widthDevTools);
 
 sagaMiddleware.run(mySaga);
 
