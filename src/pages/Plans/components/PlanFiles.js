e93a4f5e932014fbf934273c0036ee57dc45ea39
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-08-30 23:19:49 +0200

    feat(Plans): add header to the files tab

--- src/pages/Plans/components/PlanFiles.js
+++ src/pages/Plans/components/PlanFiles.js
@@ -4,7 +4,7 @@ import { object, bool } from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 import { triggerFiles } from 'actions/repository';
-import { Table, Tag, Divider, Icon } from 'antd';
+import { Table, Tag, Divider, Icon, Button, Menu, Dropdown } from 'antd';
 import { readableDate } from 'utils';
 
 const columns = [
@@ -12,6 +12,7 @@ const columns = [
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
+    sorter: (a, b) => a.name.length - b.name.length,
     render: (text, record) => (
       <Fragment>
         <Icon 
@@ -26,6 +27,7 @@ const columns = [
     title: 'Last Modified',
     key: 'timestamp',
     dataIndex: 'date',
+    sorter: (a, b) => new Date(b) - new Date(a),
     render: date => (
       <Tag color="geekblue">
         { readableDate(date) }
@@ -45,6 +47,27 @@ const columns = [
   },
 ];
 
+const menu = (
+  <Menu>
+    <Menu.Item key="1">
+      <Icon type="save"/>
+      Save Changes
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Icon type="monitor"/>
+      Preview
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Icon type="pull-request"/>
+      Merge
+    </Menu.Item>
+    <Menu.Item key="4">
+      <Icon type="fork"/>
+      Add Draft
+    </Menu.Item>
+  </Menu>
+);
+
 class PlanFiles extends PureComponent {
   static propTypes = {
     plan: object,
@@ -63,6 +86,22 @@ class PlanFiles extends PureComponent {
 
     return (
       <div className="plans__files">
+        <div className="plans__files-menu">
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button icon="branches">
+              Drafts <Icon type="down" />
+            </Button>
+          </Dropdown>
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button icon="build">
+              Actions <Icon type="down" />
+            </Button>
+          </Dropdown>
+        </div>
+        <div className="plans__files-edit">
+          <Button icon="file-add">Add file</Button>&nbsp;&nbsp;
+          <Button icon="folder-add">Add folder</Button>
+        </div>
         <Table 
           columns={columns} 
           dataSource={repoFiles.sort((a, b) => b.isDirectory - a.isDirectory)} 
